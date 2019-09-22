c0a4a0da0079d046677cd788aa982ccdc024d955
Jing Li <lyovse@gmail.com>
2019-09-22 16:59:29 +1000

    Set username, key and cart in session

---
+++ server/login.php
@@ -0,0 +1,21 @@
+<?php session_start();
+require 'utils.php';
+
+$user = $_POST['user'];
+$pwd = $_POST['pwd'];
+$key = $_POST['key'];
+$record = $user . ',' . $pwd;
+
+if (!user_exists($user)) {
+    echo '<p>Incorrect Username.</p><br>';
+    echo '<a href="../client/login.html">Try Again.</a>';
+} elseif (verify_login($record)) {
+    $_SESSION['user'] = $user;
+    $_SESSION['key'] = rsa_decrypt($key);
+    $cart_content = get_cart_content($user);
+    $_SESSION['cart'] = $cart_content;
+    redirect('../client/cart.php');
+} else {
+    echo '<p>Incorrect Password.</p><br>';
+    echo '<a href="../client/login.html">Try Again.</a>';
+}
