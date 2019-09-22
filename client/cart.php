63845ecef044fdc2f25c7b7fd4953074e588bfbd
Jing Li <lyovse@gmail.com>
2019-09-22 21:45:37 +1000

    Add cart form structure

--- client/cart.php
+++ client/cart.php
@@ -7,6 +7,7 @@ $products = explode(',', $cart);
 echo '<h1>Hi ', $user, '! Here Is Your Shopping Cart!</h1>';
 
 echo '
+<form>
 <table>
     <tr>
         <th>Product</th>
@@ -22,8 +23,8 @@ foreach ($products as $product) {
     echo "
     <tr>
         <td>{$name}</td>
-        <td id='p{$i}'>{$price}</td>
-        <td><input id='p{$i}q' name='p{$i}q' type='number' value='{$quantity}' min='0' max='10' aria-labelledby='quant'></td>
+        <td id='p{$i}p'>{$price}</td>
+        <td><input id='p{$i}q' name='p{$i}q' type='number' value='{$quantity}' min='0' max='10' aria-labelledby='quant' oninput='update(\"$i\")'></td>
         <td><input id='p{$i}s' name='p{$i}s' readonly value='0'></td>
     </tr>";
     $i++;
@@ -38,8 +39,35 @@ echo "
         <td id='total_p'>{$total_p}</td>
     </tr>
 </table>";
+
+echo '<br>';
+echo "<label>Credit Card Number: <input id='cc' name='cc' type='text'></label>";
+echo '<br><br>';
+echo "<button id='submit' type='submit' onclick='processSubmit()'>Submit</button>";
+echo '</form>';
 ?>
 
 <script>
+    function update(prodNum) {
+        updateSubTotal(prodNum);
+    }
 
+    function updateSubTotal(prodNum) {
+        let idBase = 'p' + prodNum;
+        let priceID = idBase + 'p';
+        let quantID = idBase + 'q';
+        let subTotalID = idBase + 's';
+        let priceValue = Number(document.getElementById(priceID).innerText);
+        let quantityValue = Number(document.getElementById(quantID).value);
+        let subTotal = priceValue * quantityValue;
+        document.getElementById(subTotalID).value = subTotal;
+    }
+
+    function updateTotal() {
+
+    }
+
+    function processSubmit() {
+
+    }
 </script>
