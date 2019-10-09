e6d9d2a75ad959b94119b9a5e3cbb016d28ea762
Jing Li <lyovse@gmail.com>
2019-10-09 12:03:48 +1100

    Show order confirmation

--- server/save.php
+++ server/save.php
@@ -18,9 +18,51 @@ save_credit_card($user, $credit_card);
 echo "
     <h1>Thank you!</h1>
     <h2>Your shopping cart and credit card have been saved!</h2>
-    <p>You may logout now.</p>
-    <button onclick='logout()'>Logout</button>
+    <p>The following is your order confirmation:</p>
 ";
+
+$products = explode(',', $content);
+echo "
+<table>
+    <tr>
+        <th>Product</th>
+        <th>Price</th>
+        <th>Quantity</th>
+        <th>Subtotal</th>
+    </tr>
+    ";
+$index = 0;
+$num_of_products = count($products);
+$total_q = 0;
+$total_p = 0;
+foreach ($products as $product) {
+    list($name, $price, $quantity) = explode(':', $product);
+    $subtotal = $price * $quantity;
+    echo "
+    <tr>
+        <td>{$name}</td>
+        <td>{$price}</td>
+        <td>{$quantity}</td>
+        <td>{$subtotal}</td>
+    </tr>
+    ";
+    $index++;
+    $total_q += $quantity;
+    $total_p += $price * $quantity;
+}
+echo "
+    <tr>
+        <td>Total</td>
+        <td></td>
+        <td>{$total_q}</td>
+        <td>{$total_p}</td>
+    </tr>
+</table>";
+
+echo "
+<p>You may logout now.</p>
+<button onclick='logout()'>Logout</button>
+"
 ?>
 
 <script>
